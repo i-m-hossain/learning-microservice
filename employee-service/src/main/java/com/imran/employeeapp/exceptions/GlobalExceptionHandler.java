17d4c14fd4dac163febf39e0ffbee836319633f3
mdimran1409036 <mdimran1409036@gmail.com>
2024-04-20 02:25:55 +0600

    global exception added

---
+++ employee-service/src/main/java/com/imran/employeeapp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.imran.employeeapp.exceptions;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(createErrorResponse(400, errors));
+    }
+
+    private Map<String, Object> createErrorResponse(int status, Map<String, Object> errors) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("errors", errors);
+        response.put("status", status);
+        return response;
+    }
+}
